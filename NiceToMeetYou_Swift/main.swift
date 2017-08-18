47e1403f20bd579203dd6b5482d9c3d929e4b7c2
leonguevara <leon.guevara@outlook.com>
2017-08-18 12:41:42 -0500

    Initial commit

--- NiceToMeetYou_Swift/main.swift
+++ NiceToMeetYou_Swift/main.swift
@@ -2,11 +2,17 @@
 //  main.swift
 //  NiceToMeetYou_Swift
 //
-//  Created by León Felipe Guevara Chávez on 5/22/17.
+//  This program will ask the user for their name and it will greet them
+//  using their name.
+//
+//  Created by León Felipe Guevara Chávez on 5/20/17.
 //  Copyright © 2017 León Felipe Guevara Chávez. All rights reserved.
+//  email: leon.guevara@itesm.mx
 //
 
-import Foundation
+//  import Foundation   /* I commented this line because we do not need it */
 
-print("Hello, World!")
-
+print("What is your name?")     // We ask for the user's name
+let userName = readLine()       // We define userName and collect input with readLine
+let message : String = "Hello \(userName!)! Nice to meet you."  // We build the greeting message
+print(message)                  // We display the greeting message
